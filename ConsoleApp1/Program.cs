f3fb60a0fc835d74a830c4f0a573a86c658e61e3
Hovva <35590736+Hovva@users.noreply.github.com>
2024-08-05 12:04:22 +0300

    убрал либу с решениями литкод

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,7 +1,6 @@
 ﻿
 
 using System.ComponentModel.Design;
-using ClassLibrary1;
 using GeometryLibrary;
 
 List<IFigure> figures = new List<IFigure>()
@@ -34,47 +33,5 @@ foreach (var v in figures)
 }
 
 
-
-
-
-
-//removeDuplicates();
-//MaxProfit();
-//Rotate();
-//ContainsDuplicate();
-
-//Arrays.PlusOne([9]);
-//
-// static void ContainsDuplicate()
-// {
-//     int[] nums = new[] { 1,1,1,3,3,4,3,2,4,2 };
-//     bool ans = Arrays.ContainsDuplicate(nums);
-//     Console.WriteLine(ans?"true":"false");
-// }
-// static void Rotate()
-// {
-//     int[] nums = new[] { -1, -100, 3, 99 };
-//     Arrays.Rotate(nums, 2);
-//     foreach (var v in nums)
-//         Console.Write(v + " ");
-// }
-// static void removeDuplicates()
-// {
-//     int[] nums = [1, 1, 2]; // Input array
-//     int[] expectedNums = [1, 2]; // The expected answer with correct length
-//
-//     int k = Arrays.RemoveDuplicates(nums); // Calls your implementation
-//
-//     Console.WriteLine(k);
-//     for (int i = 0; i < k; i++) {
-//         Console.Write(nums[i] + ",");
-//     }
-// }
-//
-// static void MaxProfit()
-// {
-//     int ans = Arrays.MaxProfit(new[] { 1,2,4,3,6});
-//     Console.WriteLine(ans);
-// }
 
 
